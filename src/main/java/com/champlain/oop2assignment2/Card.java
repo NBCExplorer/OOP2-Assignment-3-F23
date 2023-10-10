8fb99d1b275d028312d60ae5d09466217dfb674a
Hamza Mahi <le2029384@crc-lennox.qc.ca>
2023-10-09 20:16:11 -0400

    Added equals method feature

--- src/main/java/com/champlain/oop2assignment2/Card.java
+++ src/main/java/com/champlain/oop2assignment2/Card.java
@@ -1,11 +1,12 @@
 package com.champlain.oop2assignment2;
 
+import java.util.Objects;
+
 public class Card {
     private final Suit aSuit;
-
     private final Rank aRank;
 
-    public Card (Rank pRank, Suit pSuit) {
+    public Card(Rank pRank, Suit pSuit) {
         this.aRank = pRank;
         this.aSuit = pSuit;
     }
@@ -21,5 +22,18 @@ public class Card {
     @Override
     public String toString() {
         return this.aRank + " of " + this.aSuit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Card otherCard = (Card) obj;
+        return aSuit == otherCard.aSuit && aRank == otherCard.aRank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(aSuit, aRank);
     }
 }
